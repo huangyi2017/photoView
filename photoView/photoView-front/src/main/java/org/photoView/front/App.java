f8851400fde1a985709ba3f1b1b71e41c6386681
huangyi <huangyi@gdbigdata.com>
2017-08-31 14:16:38 +0800

    提交front project

---
+++ photoView/photoView-front/src/main/java/org/photoView/front/App.java
@@ -0,0 +1,13 @@
+package org.photoView.front;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
